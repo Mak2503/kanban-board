07ed1e399b55b6183b4f6d2456c5a2259f41ab97
Mak2503 <mak2503sde@gmail.com>
2024-08-22 14:47:12 +0530

    add board icon

---
+++ src/components/BoardIcon.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+type Props = {
+  border?: string;
+  bg?: string;
+};
+
+const BoardIcon = ({ border = "#858fa1", bg = "#2C2C38" }: Props) => {
+  return (
+    <div className="w-4 h-4 mr-3">
+      <svg viewBox="0 0 240 240" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="240" height="240" rx="16" fill={border} />
+        <rect
+          x="10"
+          y="10.9955"
+          width="220"
+          height="219.005"
+          rx="16"
+          fill={bg}
+        />
+        <line
+          y1="-5"
+          x2="135.976"
+          y2="-5"
+          transform="matrix(0.999974 -0.00725502 0.00435017 0.999991 10 123.987)"
+          stroke={border}
+          stroke-width="10"
+        />
+        <line
+          x1="145.972"
+          y1="62.7375"
+          x2="230"
+          y2="62.7375"
+          stroke={border}
+          stroke-width="10"
+        />
+        <line
+          x1="145.972"
+          y1="174.231"
+          x2="230"
+          y2="174.231"
+          stroke={border}
+          stroke-width="10"
+        />
+        <line
+          x1="140.972"
+          y1="230"
+          x2="140.972"
+          y2="10"
+          stroke={border}
+          stroke-width="10"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default BoardIcon;
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,32 +2,37 @@
 
 import Logo from "./Logo";
 import kanbanData from "@/constants/data.json";
+import BoardIcon from "./BoardIcon";
 
 const SideBar = () => {
   return (
     <div className="p-8 w-1/4 h-screen bg-secondaryBg border-r border-r-[#363642]">
       <Logo />
       <div className="mt-12">
-        <h1
-          className="text-xs text-secondaryText"
-          style={{ letterSpacing: "3px" }}
-        >
+        <h1 className="text-xs text-secondaryText tracking-widest font-semibold">
           ALL BOARDS ({kanbanData.data.length})
         </h1>
-        {/* 
-          TODO: 
-          1. Add Board Icon
-          2. Implement board selection functionality
-        */}
-        <div className="mt-4 text-sm">
-          {kanbanData.data.map((data) => (
+        {/* TODO: Implement board selection functionality */}
+        <div className="mt-4">
+          {kanbanData.data.map((data, index) => (
             <div
               key={data.id}
-              className={`p-3 pl-8 -ml-8 first:text-primaryText text-secondaryText first:bg-primary first:rounded-r-3xl`}
+              className={`flex items-center p-3 pl-8 -ml-8 first:text-primaryText font-semibold text-secondaryText first:bg-primary first:rounded-r-3xl`}
             >
-              {data.name}
+              {index === 0 ? (
+                <BoardIcon border="#feffff" bg="#645fc6" />
+              ) : (
+                <BoardIcon />
+              )}
+              <span>{data.name}</span>
             </div>
           ))}
+          <div
+            className={`flex items-center p-3 pl-8 -ml-8 text-primary font-semibold`}
+          >
+            <BoardIcon border="#645fc6" />
+            <span>+ Create New Board</span>
+          </div>
         </div>
       </div>
     </div>
