608d272f4a5abf3d996743a74a314a45b8c4d13f
Mak2503 <mak2503sde@gmail.com>
2024-08-22 14:46:51 +0530

    add logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,20 @@
 "use client";
 
+import Image from "next/image";
+
 const Logo = () => {
-  // TODO: Need to add logo
-  return <h1 className="text-2xl font-bold text-primaryText">kanban</h1>;
+  return (
+    <div className="flex items-center">
+      <Image
+        src="/logo.svg"
+        alt="logo"
+        width={20}
+        height={20}
+        className="mr-3"
+      />
+      <h1 className="text-2xl font-bold text-primaryText">kanban</h1>
+    </div>
+  );
 };
 
 export default Logo;
